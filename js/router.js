0e7bad9ff71309688fc0bd29a506170c8bb06270
RoyerD <royer.diaz@outlook.com>
2015-05-19 15:57:10 -0500

    repair of the main Viewsand add the animal seeker

--- js/router.js
+++ js/router.js
@@ -1,10 +1,15 @@
 Pet_Mate.Router.map(function() {
+ this.resource('about');
+ this.resource('contact', function() {
+	this.resource('adresses');
+    this.resource('map');
+  });
+
  this.resource('home', {path:'/'});
  this.resource('find');	
- this.resource('about');
 
 
- this.resource('for_sale', function(){
+ this.resource('for_sale', function() {
 	this.resource('buy_birds');
 	this.resource('buy_dogs');
 	this.resource('buy_cats');
@@ -12,7 +17,7 @@ Pet_Mate.Router.map(function() {
 	this.resource('buy_exotic');
   });
 
- this.resource('for_breed', function(){
+ this.resource('for_breed', function() {
 	this.resource('stud_birds');
 	this.resource('stud_dogs');
 	this.resource('stud_cats');
@@ -20,12 +25,18 @@ Pet_Mate.Router.map(function() {
 	this.resource('stud_exotic');
   });
 
- this.resource('contact', function(){
-	this.resource('adresses');
-    this.resource('map');
-  });
+this.resource('from_farm', function() {
+	this.resource('farm_birds');
+	this.resource('farm_cows');
+	this.resource('farm_exotic');
+	this.resource('farm_horses');
+	this.resource('farm_pigs');
+   
+});
+
  this.resource('health');
  this.resource('pet_new');
+ this.resource('find');
 
 });
 
