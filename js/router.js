6370479b4755b0a7b40ed04441c89f827183a1c4
RoyerD <royer.diaz@outlook.com>
2015-05-19 15:27:59 -0500

    adding other templates before controllers

--- js/router.js
+++ js/router.js
@@ -2,6 +2,30 @@ Pet_Mate.Router.map(function() {
  this.resource('home', {path:'/'});
  this.resource('find');	
  this.resource('about');
- this.resource('contact');
+
+
+ this.resource('for_sale', function(){
+	this.resource('buy_birds');
+	this.resource('buy_dogs');
+	this.resource('buy_cats');
+	this.resource('buy_reptiles');
+	this.resource('buy_exotic');
+  });
+
+ this.resource('for_breed', function(){
+	this.resource('stud_birds');
+	this.resource('stud_dogs');
+	this.resource('stud_cats');
+	this.resource('stud_reptiles');
+	this.resource('stud_exotic');
+  });
+
+ this.resource('contact', function(){
+	this.resource('adresses');
+    this.resource('map');
+  });
+ this.resource('health');
+ this.resource('pet_new');
+
 });
 
